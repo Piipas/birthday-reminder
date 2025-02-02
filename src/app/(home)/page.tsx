3d29fb9a679685c6171d46d8b057e9409db78480
Pipas <ismailpipas@gmail.com>
2025-02-02 09:26:22 +0100

    Add Prettier for code formatting and enhance authentication button functionality

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,12 +1,28 @@
 import AuthButton from "@/components/auth-button";
+import { auth } from "@/lib/auth";
 import { Github } from "lucide-react";
+import { headers } from "next/headers";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth.api.getSession({ headers: await headers() });
+
   return (
     <div className="flex items-center justify-center h-screen">
-      <AuthButton provider="github">
-        <Github /> Continue With Github
-      </AuthButton>
+      <div className="space-y-4">
+        <div className="text-6xl font-extrabold text-center">Birthday Reminder</div>
+        <div className="text-2xl text-gray-400 text-center">Never miss a special day again!</div>
+        {session?.user ? (
+          <div className="text-center">
+            Hi {session.user.name}. <AuthButton action="signout">Logout</AuthButton>
+          </div>
+        ) : (
+          <div className="w-full flex justify-center pt-4">
+            <AuthButton provider="github" action="auth" className="mx-auto text-lg">
+              <Github className="!size-6" /> Continue With Github
+            </AuthButton>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
--- src/components/auth-button.tsx
+++ src/components/auth-button.tsx
@@ -1,18 +1,45 @@
-"use client"
+"use client";
 
-import { authClient } from "@/lib/auth-client"
-import { Button } from "./ui/button"
-import { cn } from "@/lib/utils"
+import { authClient } from "@/lib/auth-client";
+import { Button } from "./ui/button";
+import { cn } from "@/lib/utils";
+import { useRouter } from "next/navigation";
 
-const AuthButton = ({provider, children, className}: {provider: "github" | "google", children: React.ReactNode | React.ReactNode[], className?: string}) => {
+const AuthButton = ({
+  provider,
+  action,
+  children,
+  className,
+}: {
+  provider?: "github" | "google";
+  action: "auth" | "signout";
+  children: React.ReactNode | React.ReactNode[];
+  className?: string;
+}) => {
+  const router = useRouter();
   const handleOAuth2 = async () => {
-    await authClient.signIn.social({ provider  })
-  }
+    if (provider) await authClient.signIn.social({ provider });
+  };
+
+  const handleLogout = async () => {
+    await authClient.signOut({
+      fetchOptions: {
+        onSuccess: () => {
+          router.push("/");
+        },
+      },
+    });
+  };
+
   return (
-    <Button onClick={handleOAuth2} className={cn(className)}>
+    <Button
+      onClick={action === "auth" ? handleOAuth2 : handleLogout}
+      className={cn("text-lg py-6", className)}
+      size={"lg"}
+    >
       {children}
     </Button>
-  )
-}
+  );
+};
 
-export default AuthButton
+export default AuthButton;

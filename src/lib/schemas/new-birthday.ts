90317221594e80c253daaa66a4d5dff02f4f93fa
Pipas <ismailpipas@gmail.com>
2025-12-17 22:44:14 +0100

    fix: remove unused date regex from NewBirthday schema

--- src/lib/schemas/new-birthday.ts
+++ src/lib/schemas/new-birthday.ts
@@ -1,7 +1,5 @@
 import { z } from "zod";
 
-const DATE_REGEX = /^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])$/;
-
 export const NewBirthday = z.object({
   name: z.string().min(1, "Name is required").max(100, "Name is too long"),
   date: z.date({ required_error: "A date of birth is required." }),
